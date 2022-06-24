e1d638c6e827d9e277417433e83a7869bc8697df
sabriVorpagel <sabrivorpagel2512@gmail.com>
2022-06-24 17:04:56 -0300

    subo los cambios en el header, main y footer

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const app = express();
 const path = require('path')
 const port = 3030;
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
+app.use(express.static('public'))
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
+app.get('/ofertas', (req,res) => res.sendFile(path.join(__dirname, 'views', 'ofertas.html')))
+app.get('/tiendas-oficiales', (req,res) => res.sendFile(path.join(__dirname, 'views', 'tiendas-oficiales.html')))
+app.get('/vender', (req,res) => res.sendFile(path.join(__dirname, 'views', 'vender.html')))
+app.get('/ayuda', (req,res) => res.sendFile(path.join(__dirname, 'views', 'ayuda.html')))
+app.get('/crea-tu-cuenta', (req,res) => res.sendFile(path.join(__dirname, 'views', 'crea-tu-cuenta.html')))
+app.get('/ingresa', (req,res) => res.sendFile(path.join(__dirname, 'views', 'ingresa.html')))
+app.get('/mis-compras', (req,res) => res.sendFile(path.join(__dirname, 'views', 'mis-compras.html')))
 
 
 
